48826182f36b3e4ca8fd1cf0d16b5cb8cab0e261
Brit Butler <redline6561@gmail.com>
2016-11-16 11:18:22 -0500

    Add the form submit handler to the Add Controller.

--- src/js/controllers/add.js
+++ src/js/controllers/add.js
@@ -1,6 +1,14 @@
-function AddController ($scope, $http) {
-  
+import { SERVER } from "../server";
+
+function AddController ($scope, $http, $state) {
+
+  $scope.addGif = (gif) => {
+    $http.post(SERVER, gif).then((resp) => {
+      $state.go('home');
+    });
+  };
+
 };
 
-AddController.$inject = ['$scope', '$http'];
+AddController.$inject = ['$scope', '$http', '$state'];
 export { AddController };
